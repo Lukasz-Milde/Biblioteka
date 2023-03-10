5107da6bbb35a583b6b27fb6f596750059b9d85b
Lukasz-Milde <lukasz@milde.live>
2023-03-10 22:26:20 +0100

    rent return kind of done

--- dist/index.js
+++ dist/index.js
@@ -1,40 +1,55 @@
 "use strict";
 class User {
-	name;
-	surname;
-	uuid;
-	constructor(name, surname, uuid) {
-		this.name = name;
-		this.surname = surname;
-		this.uuid = uuid;
-	}
+    name;
+    surname;
+    uuid;
+    constructor(name, surname, uuid) {
+        this.name = name;
+        this.surname = surname;
+        this.uuid = uuid;
+    }
 }
 class Book {
-	title;
-	author;
-	uuid;
-	randomPic;
-	description;
-	dupa;
-	constructor(title, author, uuid, randomPic, description, dupa) {
-		this.title = title;
-		this.author = author;
-		this.uuid = uuid;
-		this.randomPic = randomPic;
-		this.description = description;
-		this.dupa = dupa;
-	}
+    title;
+    author;
+    uuid;
+    randomPic;
+    description;
+    constructor(title, author, uuid, randomPic, description) {
+        this.title = title;
+        this.author = author;
+        this.uuid = uuid;
+        this.randomPic = randomPic;
+        this.description = description;
+        this.title = title;
+        this.author = author;
+        this.uuid = uuid;
+        this.randomPic = randomPic;
+        this.description = description;
+    }
 }
-
-const lukasz = new User("lukasz", "milde", "124iudhbcujd");
-const ksiazka = new Book(
-	"lal",
-	"dupek",
-	"145jkgh",
-	"pic",
-	"wihdlskhd",
-	"dupeczka"
-);
-
-console.log(lukasz);
-console.log(ksiazka);
+class Booking {
+    user;
+    bookingDate;
+    returnDate;
+    rentedBooks;
+    lateFee;
+    FeePerDay = 5;
+    constructor(user) {
+        this.user = user;
+        this.bookingDate = new Date();
+        this.returnDate = new Date();
+        this.returnDate.setDate(this.bookingDate.getDate() + 7);
+        this.rentedBooks = [];
+        this.lateFee = 0;
+    }
+    rentBook(book) {
+        this.rentedBooks.push(book);
+    }
+    returnBook(book) {
+        const indexOfBook = this.rentedBooks.indexOf(book);
+        if (indexOfBook !== -1) {
+            this.rentedBooks.splice(indexOfBook, 1);
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,28 @@ class User {
 	) {}
 }
 
-class Book {
+interface BookType {
+	title: string;
+	author: string;
+	uuid: string;
+	randomPic: string;
+	description: string;
+}
+
+class Book implements BookType {
 	constructor(
-		private title: string,
-		private author: string,
-		private uuid: string,
-		private randomPic: string,
-		private description: string,
-		private dupa: string
-	) {}
+		public title: string,
+		public author: string,
+		public uuid: string,
+		public randomPic: string,
+		public description: string
+	) {
+		this.title = title;
+		this.author = author;
+		this.uuid = uuid;
+		this.randomPic = randomPic;
+		this.description = description;
+	}
 }
 
 class Booking {
@@ -26,6 +39,19 @@ class Booking {
 
 	constructor(private user: string) {
 		this.bookingDate = new Date();
-		this.returnDate = 
+		this.returnDate = new Date();
+		this.returnDate.setDate(this.bookingDate.getDate() + 7);
+		this.rentedBooks = [];
+		this.lateFee = 0;
+	}
+
+	rentBook(book: any): void {
+		this.rentedBooks.push(book);
+	}
+	returnBook(book: any): void {
+		const indexOfBook = this.rentedBooks.indexOf(book);
+		if (indexOfBook !== -1) {
+			this.rentedBooks.splice(indexOfBook, 1);
+		}
 	}
 }
